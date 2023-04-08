7c5dd2e38d721ee6b486d2c0c21e409244c2b691
Caleb <calebfunderburk@icloud.com>
2023-04-08 14:41:54 -0400

    navbar mobile responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import { AppBar, Toolbar, IconButton, Stack, Typography, Button, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, useMediaQuery } from "@mui/material"
+import { AppBar, Toolbar, IconButton, Stack, Typography, Button, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, useMediaQuery } from '@mui/material'
+import { Menu, Close } from '@mui/icons-material'
 
 const drawerWidth = 240
 
 const Header = (props) => {
 
-    const [ open, setOpen ] = useState(false)
+    const [open, setOpen] = useState(false)
+
+    const mobileResponse = useMediaQuery('(max-width:600px)')
 
     const { window } = props
 
@@ -15,12 +18,20 @@ const Header = (props) => {
 
     const drawer = (
         <List>
-            <Divider/>
-            <Button color='inherit' onClick={close}>Skills</Button>
-            <Button color='inherit' onClick={close}>Projects</Button>
-            <Button color='inherit' onClick={close}>Resume</Button>
-            <Button color='inherit' onClick={close}>Contact</Button>
-            <Divider/>
+            <Divider />
+            <ListItem>
+                <Button color='inherit' onClick={close}>Skills</Button>
+            </ListItem>
+            <ListItem>
+                <Button color='inherit' onClick={close}>Projects</Button>
+            </ListItem>
+            <ListItem>
+                <Button color='inherit' onClick={close}>Resume</Button>
+            </ListItem>
+            <ListItem>
+                <Button color='inherit' onClick={close}>Contact</Button>
+            </ListItem>
+            <Divider />
         </List>
     )
 
@@ -28,35 +39,59 @@ const Header = (props) => {
 
     return (
         <>
-            <AppBar position='fixed' sx={{ zIndex: 1201 }}>
-                <Toolbar>
-                    <Typography sx={{ flexGrow: 1 }}>Caleb Funderburk</Typography>
-                    <Stack direction='row' spacing={{ xs: 1, sm: 2, md: 3 }}>
-                        <Button color='inherit'>Skills</Button>
-                        <Button color='inherit'>Projects</Button>
-                        <Button color='inherit'>Resume</Button>
-                        <Button color='inherit'>Contact</Button>
-                    </Stack>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                container={container}
-                anchor='right'
-                variant='temporary'
-                open={open}
-                onClose={close}
-                ModalProps={{
-                    keepMounted: true
-                }}
-                sx={{
-                    display: { xs: 'block', sm: 'none' },
-                    '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
-                }}
-            >
-            <Toolbar/>
-            <Divider/>
-
-            </Drawer>
+            {!mobileResponse ?
+                <>
+                    <AppBar position='fixed' sx={{ zIndex: 1201 }}>
+                        <Toolbar>
+                            <Typography sx={{ flexGrow: 1 }}>Caleb Funderburk</Typography>
+                            <Stack direction='row' spacing={{ xs: 1, sm: 2, md: 3 }}>
+                                <Button color='inherit'>Skills</Button>
+                                <Button color='inherit'>Projects</Button>
+                                <Button color='inherit'>Resume</Button>
+                                <Button color='inherit'>Contact</Button>
+                            </Stack>
+                        </Toolbar>
+                    </AppBar>
+                </>
+            :
+                <>
+                    <AppBar position='fixed' sx={{ zIndex: 1201 }}>
+                        <Toolbar>
+                            <Typography sx={{ flexGrow: 1 }}>Caleb Funderburk</Typography>
+                            <Stack direction='row' spacing={{ xs: 1, sm: 2, md: 3 }}>
+                                {!open ?
+                                    <IconButton>
+                                        <Menu color='inherit' onClick={setOpen}/>
+                                    </IconButton>
+                                :
+                                    <IconButton>
+                                        <Close color='inherit' onClick={close}/>
+                                    </IconButton>
+                                }
+                            </Stack>
+                        </Toolbar>
+                    </AppBar>
+                    <Drawer
+                        container={container}
+                        variant='temporary'
+                        anchor='right'
+                        open={open}
+                        onClose={close}
+                        PaperProps={{ elevation: 6 }}
+                        ModalProps={{
+                            keepMounted: true
+                        }}
+                        sx={{
+                            display: { xs: 'block', sm: 'none' },
+                            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth }
+                        }}
+                    >
+                        <Toolbar />
+                        <Divider />
+                        {drawer}
+                    </Drawer>
+                </>
+            }
         </>
     )
 }
