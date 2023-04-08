13fa0257b2e880966617012f712f096dedb902b9
Caleb <calebfunderburk@icloud.com>
2023-04-08 15:26:53 -0400

    added socials to navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { AppBar, Toolbar, IconButton, Stack, Typography, Button, Drawer, Divider, List, ListItem, ListItemIcon, ListItemText, useMediaQuery } from '@mui/material'
-import { Menu, Close } from '@mui/icons-material'
+import { Menu, Close, LinkedIn, GitHub, YouTube } from '@mui/icons-material'
 
 const drawerWidth = 240
 
@@ -22,16 +22,30 @@ const Header = (props) => {
             <ListItem>
                 <Button color='inherit' onClick={close}>Skills</Button>
             </ListItem>
+            <Divider />
             <ListItem>
                 <Button color='inherit' onClick={close}>Projects</Button>
             </ListItem>
+            <Divider />
             <ListItem>
                 <Button color='inherit' onClick={close}>Resume</Button>
             </ListItem>
+            <Divider />
             <ListItem>
                 <Button color='inherit' onClick={close}>Contact</Button>
             </ListItem>
             <Divider />
+            <Stack direction='row' justifyContent='space-around'>
+                <IconButton>
+                    <LinkedIn fontSize='large' />
+                </IconButton>
+                <IconButton>
+                    <GitHub fontSize='large' />
+                </IconButton>
+                <IconButton>
+                    <YouTube fontSize='large' />
+                </IconButton>
+            </Stack>
         </List>
     )
 
@@ -44,28 +58,41 @@ const Header = (props) => {
                     <AppBar position='fixed' sx={{ zIndex: 1201 }}>
                         <Toolbar>
                             <Typography sx={{ flexGrow: 1 }}>Caleb Funderburk</Typography>
-                            <Stack direction='row' spacing={{ xs: 1, sm: 2, md: 3 }}>
+                            <Stack direction='row' spacing={{ sm: 1, md: 4 }}>
                                 <Button color='inherit'>Skills</Button>
                                 <Button color='inherit'>Projects</Button>
                                 <Button color='inherit'>Resume</Button>
                                 <Button color='inherit'>Contact</Button>
                             </Stack>
                         </Toolbar>
+                        <Toolbar variant='dense'>
+                            <Stack sx={{ flexGrow: 2 }} direction='row' spacing={5} justifyContent='flex-end'>
+                                <IconButton>
+                                    <LinkedIn fontSize='large' />
+                                </IconButton>
+                                <IconButton>
+                                    <GitHub fontSize='large' />
+                                </IconButton>
+                                <IconButton>
+                                    <YouTube fontSize='large' />
+                                </IconButton>
+                            </Stack>
+                        </Toolbar>
                     </AppBar>
                 </>
-            :
+                :
                 <>
                     <AppBar position='fixed' sx={{ zIndex: 1201 }}>
                         <Toolbar>
                             <Typography sx={{ flexGrow: 1 }}>Caleb Funderburk</Typography>
                             <Stack direction='row' spacing={{ xs: 1, sm: 2, md: 3 }}>
                                 {!open ?
-                                    <IconButton>
-                                        <Menu color='inherit' onClick={setOpen}/>
+                                    <IconButton onClick={setOpen}>
+                                        <Menu color='inherit' />
                                     </IconButton>
-                                :
-                                    <IconButton>
-                                        <Close color='inherit' onClick={close}/>
+                                    :
+                                    <IconButton onClick={close}>
+                                        <Close color='inherit' />
                                     </IconButton>
                                 }
                             </Stack>
