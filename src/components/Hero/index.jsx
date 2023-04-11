83226d1b5dceba47c407754d7e0a7c3f75f8aee4
Caleb <calebfunderburk@icloud.com>
2023-04-10 20:55:28 -0400

    positioned card for hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,4 +1,4 @@
-import { Typography, Box, useMediaQuery } from '@mui/material'
+import { Typography, Box, Paper, Card, useMediaQuery } from '@mui/material'
 
 const Hero = () => {
 
@@ -7,8 +7,11 @@ const Hero = () => {
     return (
         <>
             {!mobileViewport ?
-                <Box id='hero' sx={{ backgroundColor: '#ff9800', height: '95.5vh' }}>
-                    <Typography>Hero Section</Typography>
+                <Box id='hero' display='flex' justifyContent='center' alignItems='center' sx={{ backgroundColor: '#ff9800', height: '95.5vh' }}>
+                    <Card elevation={6} sx={{ minWidth: 550, minHeight:'50vh' }}>
+                        <Typography>Welcome, I'm Caleb.</Typography>
+                        <Typography>I'm a full stack developer.</Typography>
+                    </Card>
                 </Box>
                 :
                 <Box id='hero' sx={{ backgroundColor: '#ff9800', height: '95vh' }}>
