80c3c62c743404468ed082d9c35ec972e9a56180
Caleb <calebfunderburk@icloud.com>
2023-04-11 21:25:58 -0400

    started positioning cards for Projects

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,18 +1,44 @@
-import { Typography, Box, useMediaQuery } from '@mui/material'
+import { Typography, Box, Grid, Card, Stack, useMediaQuery } from '@mui/material'
 
 const Projects = () => {
 
-    const mobileViewport = useMediaQuery('(max-width:600px)')
+    const mobileViewport = useMediaQuery('(max-width:800px)')
 
     return (
         <>
             {!mobileViewport ?
                 <Box id='projects' sx={{ backgroundColor: '#f44336', height: '95.5vh' }}>
-                    <Typography>Projects Section</Typography>
+                    <Grid container justifyContent='center' rowGap={2} columnSpacing={2} sx={{ py: '5%' }}>
+                        <Grid item xs={5}>
+                            <Card sx={{ minHeight: '40vh' }}>1</Card>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Card sx={{ minHeight: '40vh' }}>2</Card>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Card sx={{ minHeight: '40vh' }}>3</Card>
+                        </Grid>
+                        <Grid item xs={5}>
+                            <Card sx={{ minHeight: '40vh' }}>4</Card>
+                        </Grid>
+                    </Grid>
                 </Box>
                 :
                 <Box id='projects' sx={{ backgroundColor: '#f44336', height: '95vh' }}>
-                    <Typography>Projects Section</Typography>
+                    <Stack direction='column' justifyContent='center' alignItems='center' spacing={1} sx={{ py: 2 }}>
+                        <Card elevation={9} sx={{ minHeight: '30vh', minWidth: '85%' }}>
+                            <Typography>1</Typography>
+                        </Card>
+                        <Card elevation={9} sx={{ minHeight: '30vh', minWidth: '85%' }}>
+                            <Typography>2</Typography>
+                        </Card>
+                        <Card elevation={9} sx={{ minHeight: '30vh', minWidth: '85%' }}>
+                            <Typography>3</Typography>
+                        </Card>
+                        <Card elevation={9} sx={{ minHeight: '30vh', minWidth: '85%' }}>
+                            <Typography>4</Typography>
+                        </Card>
+                    </Stack>
                 </Box>
             }
         </>
