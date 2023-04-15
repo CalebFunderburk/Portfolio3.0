c6806f6dd34f1c98e52ab44e60ef9b9cfc59b01c
Caleb <calebfunderburk@icloud.com>
2023-04-15 15:15:54 -0400

    added styling and content to Hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,4 +1,5 @@
-import { Typography, Box, Card, useMediaQuery } from '@mui/material'
+import { Link } from 'react-scroll'
+import { Typography, Box, Card, Grid, ButtonGroup, Button, useMediaQuery } from '@mui/material'
 
 const Hero = () => {
 
@@ -8,10 +9,16 @@ const Hero = () => {
         <>
             {!mobileViewport ?
                 <Box id='hero' display='flex' justifyContent='center' alignItems='center' sx={{ backgroundColor: '#e0e0e0', height: '95.5vh' }}>
-                    <Card elevation={9} sx={{ minWidth: '85%', minHeight:'50vh' }}>
-                        <Typography>Welcome, I'm Caleb.</Typography>
-                        <Typography>I'm a full stack developer.</Typography>
-                    </Card>
+                    <Grid sx={{ minWidth: '85%', minHeight:'50vh' }}>
+                        <Typography variant='h1'>Welcome, I'm <Typography variant='h' color='#1976d2'>Caleb</Typography>.</Typography>
+                        <Typography variant='h1'>I'm a full stack developer.</Typography>
+                        <Link to='projects' spy={true} smooth={true} offset={-56} duration={500}>
+                            <Button variant='contained' size='large' sx={{ my: 3, mx: 1 }}>View my work</Button>
+                        </Link>
+                        <Link to='contact' spy={true} smooth={true} offset={-56} duration={500}>
+                            <Button variant='contained' size='large' sx={{ my: 3, mx: 1 }}>Contact me</Button>
+                        </Link>
+                    </Grid>
                 </Box>
                 :
                 <Box id='hero' display='flex' justifyContent='center' alignItems='center' sx={{ backgroundColor: '#e0e0e0', height: '95vh' }}>
