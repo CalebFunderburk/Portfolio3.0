a0311158921f29a1b1974c219f88e8316e1187c3
Caleb <calebfunderburk@icloud.com>
2023-04-16 15:22:41 -0400

    added labels to skills icons

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -6,6 +6,8 @@ const Skills = () => {
 
     // Notes:
     // Currently using mt to keep skills boxes away from the top of the parent container
+    // Need to replace icons that have tech name printed in or under logo
+    // Vite label is acting weird, but I think it's because all icons are sharing some sort of space
 
     return (
         <>
@@ -18,18 +20,28 @@ const Skills = () => {
                             <Grid container direction='row' justifyContent='center' alignItems='center' spacing={4} mt={5}>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\js.png' alt='javascript' width='80px' />
+                                    <br/>
+                                    <label>JavaScript</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\react.png' alt='react' width='80px' />
+                                    <br/>
+                                    <label>React</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\vite.png' alt='vite' width='80px' />
+                                    <br/>
+                                    <label>Vite</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\html.png' alt='html' width='80px' />
+                                    <br/>
+                                    <label>HTML</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\css.png' alt='css' width='90px' />
+                                    <br/>
+                                    <label>CSS</label>
                                 </Grid>
                             </Grid>
                         </Card>
@@ -37,25 +49,39 @@ const Skills = () => {
                             <Typography variant='h4' align='center'>Back End</Typography>
                             <Grid container direction='row' justifyContent='center' alignItems='center' spacing={4} mt={5}>
                                 <Grid item align='center' xs={3}>
-                                    <img src='src\assets\images\skills\node.png' alt='node' width='80px' />
+                                    <img src='src\assets\images\skills\node.png' alt='node' width='90px' height='70' />
+                                    <br/>
+                                    <label>Node</label>
                                 </Grid>
                                 <Grid item align='center' xs={3}>
                                     <img src='src\assets\images\skills\express.png' alt='express' width='80px' />
+                                    <br/>
+                                    <label>Express</label>
                                 </Grid>
                                 <Grid item align='center' xs={3}>
                                     <img src='src\assets\images\skills\mysql.png' alt='mysql' width='80px' />
+                                    <br/>
+                                    <label>MySQL</label>
                                 </Grid>
                                 <Grid item align='center' xs={3}>
                                     <img src='src\assets\images\skills\sequelize.png' alt='sequelize' width='80px' />
+                                    <br/>
+                                    <label>Sequelize</label>
                                 </Grid>
                                 <Grid item align='center' xs={3}>
                                     <img src='src\assets\images\skills\mongodb.png' alt='mongodb' width='80px' />
+                                    <br/>
+                                    <label>MongoDB</label>
                                 </Grid>
                                 <Grid item align='center' xs={3}>
                                     <img src='src\assets\images\skills\mongoose.png' alt='mongoose' width='80px' />
+                                    <br/>
+                                    <label>Mongoose</label>
                                 </Grid>
                                 <Grid item align='center' xs={3}>
                                     <img src='src\assets\images\skills\graphql.png' alt='grapql' width='80px' />
+                                    <br/>
+                                    <label>GraphQL</label>
                                 </Grid>
                             </Grid>
                         </Card>
@@ -64,21 +90,33 @@ const Skills = () => {
                             <Grid container direction='row' justifyContent='center' alignItems='center' spacing={4} mt={5}>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\git.png' alt='git' width='80px' />
+                                    <br/>
+                                    <label>Git</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\heroku.png' alt='heroku' width='80px' />
+                                    <br/>
+                                    <label>Heroku</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\jest.png' alt='jest' width='80px' />
+                                    <br/>
+                                    <label>Jest</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\webpack.png' alt='webpack' width='80px' />
+                                    <br/>
+                                    <label>Webpack</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\insomnia.png' alt='insomnia' width='80px' />
+                                    <br/>
+                                    <label>Insomnia</label>
                                 </Grid>
                                 <Grid item align='center' xs={4}>
                                     <img src='src\assets\images\skills\vscode.png' alt='vscode' width='80px' />
+                                    <br/>
+                                    <label>VS Code</label>
                                 </Grid>
                             </Grid>
                         </Card>
